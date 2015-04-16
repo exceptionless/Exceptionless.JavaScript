92c26b1d3b18622ab2f03f8eb915f2d3f0fa88a0
Blake Niemyjski <bniemyjski@gmail.com>
2015-04-15 23:27:52 -0500

    some cleanup.

--- src/exceptionless.ts
+++ src/exceptionless.ts
@@ -1,6 +1,9 @@
 /// <reference path="typings/tsd.d.ts" />
 
 // TODO: We'll need a poly fill for promises.
+// TODO: Process Errors
+// TODO: Handle Server Settings
+
 module Exceptionless {
   export class ExceptionlessClient {
     public config:Configuration;
@@ -206,55 +209,70 @@ module Exceptionless {
         this._config.log.info('Sending ' + events.length + ' events to ' + this._config.serverUrl + '.');
         this._config.submissionClient.submit(events, this._config)
           .then(
-            (response:SubmissionResponse) => {
-              if (response.success) {
-                this._config.log.info('Sent ' + events.length + ' events to ' + this._config.serverUrl + '.');
-              } else if (response.serviceUnavailable) {
-                // You are currently over your rate limit or the servers are under stress.
-                this._config.log.error('Server returned service unavailable.');
-                this.suspendProcessing();
-                this.requeueEvents(events);
-              } else if (response.paymentRequired) {
-                // If the organization over the rate limit then discard the event.
-                this._config.log.info('Too many events have been submitted, please upgrade your plan.');
-                this.suspendProcessing(null, true, true);
-              } else if (response.unableToAuthenticate) {
-                // The api key was suspended or could not be authorized.
-                this._config.log.info('Unable to authenticate, please check your configuration. The event will not be submitted.');
-                this.suspendProcessing(15);
-              } else if (response.notFound || response.badRequest) {
-                // The service end point could not be found.
-                this._config.log.error('Error while trying to submit data: ' + response.message);
-                this.suspendProcessing(60 * 4);
-              } else if (response.requestEntityTooLarge) {
-                if (this._config.submissionBatchSize > 1) {
-                  this._config.log.error('Event submission discarded for being too large. The event will be retried with a smaller events size.');
-                  this._config.submissionBatchSize = Math.max(1, Math.round(this._config.submissionBatchSize / 1.5));
-                  this.requeueEvents(events);
-                } else {
-                  this._config.log.error('Event submission discarded for being too large. The event will not be submitted.');
-                }
-              } else if (!response.success) {
-                this._config.log.error('An error occurred while submitting events: ' + response.message);
-                this.suspendProcessing();
-                this.requeueEvents(events);
-              }
-            },
-            (response:SubmissionResponse) => {
-              this._config.log.error('An error occurred while submitting events: ' + response.message);
-              this.suspendProcessing();
-              this.requeueEvents(events);
-            })
-        .then(() => {
-          this._config.log.info('Finished processing queue.');
-          this._processingQueue = false;
-        });
+            (response:SubmissionResponse) => this.processSubmissionResponse(response, events),
+            (response:SubmissionResponse) => this.processSubmissionResponse(response, events))
+          .then(() => {
+            this._config.log.info('Finished processing queue.');
+            this._processingQueue = false;
+          });
       } catch (ex) {
         this._config.log.error('An error occurred while processing the queue: ' + ex);
         this.suspendProcessing();
-      } finally {
-        this._config.log.info('Finished processing queue.');
         this._processingQueue = false;
+      }
+    }
+
+    private processSubmissionResponse(response:SubmissionResponse, events:IEvent[]){
+      if (response.success) {
+        this._config.log.info('Sent ' + events.length + ' events to ' + this._config.serverUrl + '.');
+        return;
+      }
+
+      if (response.serviceUnavailable) {
+        // You are currently over your rate limit or the servers are under stress.
+        this._config.log.error('Server returned service unavailable.');
+        this.suspendProcessing();
+        this.requeueEvents(events);
+        return;
+      }
+
+      if (response.paymentRequired) {
+        // If the organization over the rate limit then discard the event.
+        this._config.log.info('Too many events have been submitted, please upgrade your plan.');
+        this.suspendProcessing(null, true, true);
+        return;
+      }
+
+      if (response.unableToAuthenticate) {
+        // The api key was suspended or could not be authorized.
+        this._config.log.info('Unable to authenticate, please check your configuration. The event will not be submitted.');
+        this.suspendProcessing(15);
+        return;
+      }
+
+      if (response.notFound || response.badRequest) {
+        // The service end point could not be found.
+        this._config.log.error('Error while trying to submit data: ' + response.message);
+        this.suspendProcessing(60 * 4);
+        return;
+      }
+
+      if (response.requestEntityTooLarge) {
+        if (this._config.submissionBatchSize > 1) {
+          this._config.log.error('Event submission discarded for being too large. The event will be retried with a smaller events size.');
+          this._config.submissionBatchSize = Math.max(1, Math.round(this._config.submissionBatchSize / 1.5));
+          this.requeueEvents(events);
+        } else {
+          this._config.log.error('Event submission discarded for being too large. The event will not be submitted.');
+        }
+
+        return;
+      }
+
+      if (!response.success) {
+        this._config.log.error('An error occurred while submitting events: ' + response.message);
+        this.suspendProcessing();
+        this.requeueEvents(events);
       }
     }
 
