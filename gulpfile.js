8d3f856a0495671c2953414a5454132b1274f241
Blake Niemyjski <bniemyjski@gmail.com>
2015-04-23 14:02:45 -0500

    updated build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var karma = require('gulp-karma');
-var merge2 = require('merge2');
 var rename = require('gulp-rename');
 var rimraf = require('rimraf');
 var sourcemaps = require('gulp-sourcemaps');
@@ -13,34 +12,30 @@ gulp.task('clean', function (cb) {
   rimraf('./dist', cb);
 });
 
-gulp.task('scripts', function() {
-  function fixFilePath(files){
-    for (var index = 0; index < files.length; index++) {
-      files[index] = 'src/' + files[index];
-    }
+gulp.task('typescript', function() {
+  tsProject.src('./src').pipe(gulp.dest('./dist'));
+});
 
-    return files;
-  }
-
+gulp.task('scripts', ['typescript'], function() {
   var files = [
     'node_modules/es6-promise/dist/es6-promise.js',
     'node_modules/stackframe/dist/stackframe.js',
     'node_modules/error-stack-parser/dist/error-stack-parser.js',
     'node_modules/stack-generator/dist/stack-generator.js',
     'node_modules/stacktrace-gps/dist/stacktrace-gps.js',
-    'node_modules/stacktrace-js/dist/stacktrace.js'
+    'node_modules/stacktrace-js/dist/stacktrace.js',
+    'dist/exceptionless.js'
   ];
 
   // TODO: Look into using https://www.npmjs.com/package/gulp-wrap-umd
-  var tsResult = tsProject.src('./src');
-  return merge2(gulp.src(files), tsResult)
+  return gulp.src(files)
     .pipe(sourcemaps.init())
     .pipe(concat('exceptionless.js'))
     .pipe(gulp.dest('dist'))
     .pipe(rename('exceptionless.min.js'))
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch', ['scripts'], function() {
