19aba22afbadcc9d24fb05a84d896ded754dd8e4
Frank Ebersoll <frank.ebersoll@streamteam.de>
2015-11-11 22:30:24 +0100

    Added DuplicateChecker to default plugins

--- src/plugins/EventPluginManager.ts
+++ src/plugins/EventPluginManager.ts
@@ -7,6 +7,7 @@ import { ModuleInfoPlugin } from './default/ModuleInfoPlugin';
 import { RequestInfoPlugin } from './default/RequestInfoPlugin';
 import { EnvironmentInfoPlugin } from './default/EnvironmentInfoPlugin';
 import { SubmissionMethodPlugin } from './default/SubmissionMethodPlugin';
+import { DuplicateCheckerPlugin } from './default/DuplicateCheckerPlugin';
 
 export class EventPluginManager {
   public static run(context:EventPluginContext, callback:(context?:EventPluginContext) => void): void {
@@ -43,6 +44,7 @@ export class EventPluginManager {
   public static addDefaultPlugins(config:Configuration): void {
     config.addPlugin(new ConfigurationDefaultsPlugin());
     config.addPlugin(new ErrorPlugin());
+    config.addPlugin(new DuplicateCheckerPlugin());
     config.addPlugin(new ModuleInfoPlugin());
     config.addPlugin(new RequestInfoPlugin());
     config.addPlugin(new EnvironmentInfoPlugin());
