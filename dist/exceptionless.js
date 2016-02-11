9473410980608443351486eda6de7f44af4944df
Blake Niemyjski <bniemyjski@gmail.com>
2016-02-11 14:46:01 -0600

    renamed manual stacking key
    https://github.com/exceptionless/Exceptionless/pull/187

--- dist/exceptionless.js
+++ dist/exceptionless.js
@@ -2077,7 +2077,7 @@ var EventBuilder = (function () {
     };
     EventBuilder.prototype.setManualStackingKey = function (manualStackingKey) {
         if (manualStackingKey) {
-            this.setProperty('@manual_stacking_key', manualStackingKey);
+            this.setProperty('@stack', manualStackingKey);
         }
         return this;
     };
