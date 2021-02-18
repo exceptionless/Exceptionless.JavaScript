b3b49fd8939ce25185aa8bd5241fae47e62ccb98
Blake Niemyjski <bniemyjski@gmail.com>
2021-02-18 07:31:09 -0600

    Updates to linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,21 +24,35 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
-    project: ["./tsconfig.json"],
+    project: ["./tsconfig.eslint.json"],
     tsconfigRootDir: __dirname
   },
   plugins: [
     "@typescript-eslint",
     "import",
-    "jest",
+    "jest"
   ],
   ignorePatterns: [
-    "node_modules"
+    "dist",
+    "node_modules",
+    "example"
   ],
   rules: {
+    // TODO: Fix rule errors.
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": "off"
+    "@typescript-eslint/no-for-in-array": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "no-undef": "off",
+    "no-var": "off"
   }
 };
