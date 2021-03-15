317d751ca4e1ead3a5b4cf7bc84aa3baca299995
Blake Niemyjski <bniemyjski@gmail.com>
2021-03-15 09:48:44 -0500

    Fixed build

--- packages/core/src/plugins/IEventPlugin.ts
+++ packages/core/src/plugins/IEventPlugin.ts
@@ -4,8 +4,7 @@ import { PluginContext } from "./PluginContext.js";
 export interface IEventPlugin {
   priority?: number;
   name?: string;
-  // TODO: Seems like we need a marker on startup context to determine if it's being resumed?
-  startup(context: PluginContext): Promise<void>;
-  suspend(context: PluginContext): Promise<void>;
+  startup?(context: PluginContext): Promise<void>;
+  suspend?(context: PluginContext): Promise<void>;
   run(context: EventPluginContext): Promise<void>;
 }
