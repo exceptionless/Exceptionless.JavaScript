3f9ca5e4a02ced3fdab746ad28973b8d58127c99
Blake Niemyjski <bniemyjski@gmail.com>
2021-03-30 09:00:11 -0500

    Added support for logging client self log to text area

--- example/browser/index.js
+++ example/browser/index.js
@@ -1,12 +1,14 @@
 import { Exceptionless } from "../../node_modules/@exceptionless/browser/dist/index.min.js";
 import { divide } from "./math.js";
+import { TextAreaSelfLogger } from "./text-area-logger.js";
 
 Exceptionless.startup(c => {
   c.apiKey = "LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw";
   c.serverUrl = "http://localhost:5000";
   c.updateSettingsWhenIdleInterval = 15000;
-  c.useDebugLogger();
   c.useLocalStorage();
+  c.useDebugLogger();
+  c.services.log = new TextAreaSelfLogger("logs", c.services.log);
   c.setUserIdentity("12345678", "Blake");
   c.useSessions();
 
