69f805565020ce90283169386af058abc44ec4a3
Blake Niemyjski <bniemyjski@gmail.com>
2021-04-14 19:12:15 -0500

    package updates

--- example/react-native/App.tsx
+++ example/react-native/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Exceptionless } from "@exceptionless/browser";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Exceptionless Example: { Exceptionless.config.serverUrl }</Text>
     </View>
   );
 }
