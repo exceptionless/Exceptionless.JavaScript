ca734fb221bdea3cc06eca63a1d004cb2cb63c69
Blake Niemyjski <bniemyjski@gmail.com>
2021-05-17 08:41:20 -0500

    removed example test

--- example/react/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Exceptionless react sample', () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Exceptionless React Sample/i);
-  expect(h1Element).toBeInTheDocument();
-});
