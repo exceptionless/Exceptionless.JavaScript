8fd2a711af154ef4880a639c568f7d6ae6c91247
Eric J. Smith <eric@ericjsmith.com>
2021-05-23 01:20:31 -0500

    Adding node polyfills for fetch and storage

--- example/express/app.js
+++ example/express/app.js
@@ -7,6 +7,8 @@ await Exceptionless.startup((c) => {
   c.apiKey = "LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw";
   c.serverUrl = "http://localhost:5000";
   c.useDebugLogger();
+  c.useLocalStorage();
+  c.usePersistedQueueStorage = true;
 
   c.defaultTags.push("Example", "Node");
 
