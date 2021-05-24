396bfd5601fbfc020c1f025e9db34270fff7cd6d
Blake Niemyjski <bniemyjski@gmail.com>
2021-05-24 09:04:07 -0500

    Removed IEventQueue

--- packages/core/src/configuration/Configuration.ts
+++ packages/core/src/configuration/Configuration.ts
@@ -9,7 +9,6 @@ import { ReferenceIdPlugin } from "../plugins/default/ReferenceIdPlugin.js";
 import { EventPluginContext } from "../plugins/EventPluginContext.js";
 import { IEventPlugin } from "../plugins/IEventPlugin.js";
 import { DefaultEventQueue } from "../queue/DefaultEventQueue.js";
-import { IEventQueue } from "../queue/IEventQueue.js";
 import { ISubmissionClient } from "../submission/ISubmissionClient.js";
 import { DefaultSubmissionClient } from "../submission/DefaultSubmissionClient.js";
 import { guid } from "../Utils.js";
@@ -546,5 +545,5 @@ interface IConfigurationServices {
   log: ILog;
   submissionClient: ISubmissionClient;
   storage: IStorage;
-  queue: IEventQueue;
+  queue: DefaultEventQueue;
 }
