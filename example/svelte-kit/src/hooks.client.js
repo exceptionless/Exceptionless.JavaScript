7748f557ceffc414c6f7651e7497b2f0981ee720
Blake Niemyjski <bniemyjski@gmail.com>
2023-02-16 21:38:52 -0600

    Added tags and default tags for svelte sample

--- example/svelte-kit/src/hooks.client.js
+++ example/svelte-kit/src/hooks.client.js
@@ -3,6 +3,9 @@ import { Exceptionless } from "@exceptionless/browser";
 Exceptionless.startup(c => {
   c.apiKey = "LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw";
   c.serverUrl = "http://localhost:5000";
+  c.useDebugLogger();
+
+  c.defaultTags.push("Example", "svelte-kit", "client");
 });
 
 /** @type {import('@sveltejs/kit').HandleClientError} */
--- example/svelte-kit/src/hooks.server.js
+++ example/svelte-kit/src/hooks.server.js
@@ -3,6 +3,9 @@ import { Exceptionless } from "@exceptionless/node";
 Exceptionless.startup(c => {
   c.apiKey = "LhhP1C9gijpSKCslHHCvwdSIz298twx271n1l6xw";
   c.serverUrl = "http://localhost:5000";
+  c.useDebugLogger();
+
+  c.defaultTags.push("Example", "svelte-kit", "server");
 });
 
 /** @type {import('@sveltejs/kit').HandleServerError} */
