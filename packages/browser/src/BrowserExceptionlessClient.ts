ee8143b7c6ce4c7127f067be0451868cd620aae2
Blake Niemyjski <bniemyjski@gmail.com>
2023-02-22 21:06:05 -0600

    Fixes #70: Ignore errors created from browser extensions

--- packages/browser/src/BrowserExceptionlessClient.ts
+++ packages/browser/src/BrowserExceptionlessClient.ts
@@ -2,6 +2,7 @@ import { Configuration, ExceptionlessClient, SimpleErrorPlugin } from "@exceptio
 
 import { BrowserErrorPlugin } from "./plugins/BrowserErrorPlugin.js";
 import { BrowserGlobalHandlerPlugin } from "./plugins/BrowserGlobalHandlerPlugin.js";
+import { BrowserIgnoreExtensionErrorsPlugin } from "./plugins/BrowserIgnoreExtensionErrorsPlugin.js";
 import { BrowserLifeCyclePlugin } from "./plugins/BrowserLifeCyclePlugin.js";
 import { BrowserModuleInfoPlugin } from "./plugins/BrowserModuleInfoPlugin.js";
 import { BrowserRequestInfoPlugin } from "./plugins/BrowserRequestInfoPlugin.js";
@@ -13,6 +14,7 @@ export class BrowserExceptionlessClient extends ExceptionlessClient {
       config.useLocalStorage();
 
       config.addPlugin(new BrowserGlobalHandlerPlugin());
+      config.addPlugin(new BrowserIgnoreExtensionErrorsPlugin());
       config.addPlugin(new BrowserLifeCyclePlugin());
       config.addPlugin(new BrowserModuleInfoPlugin());
       config.addPlugin(new BrowserRequestInfoPlugin());
