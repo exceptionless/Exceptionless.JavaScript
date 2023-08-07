152d0a63dec7ddfc2efbd8759e13461d3e15363b
Blake Niemyjski <bniemyjski@gmail.com>
2023-08-07 07:03:21 -0500

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-redundant-type-constituents": "off",
     "@typescript-eslint/restrict-plus-operands": "off",
   }
 };
